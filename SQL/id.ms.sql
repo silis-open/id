b63c84019f16933742e04c6631d20008881b23f5
Yanfant <446280440@qq.com>
2021-03-26 03:37:12 +0800

    move in folder SQL

---
+++ SQL/id.ms.sql
@@ -0,0 +1,17 @@
+--(c)http://gitee.com/silis/id
+
+DROP FUNCTION [dbo].[Id_GenerateChar]
+GO
+
+CREATE FUNCTION [dbo].[Id_GenerateChar]
+(
+	@rank FLOAT
+)
+RETURNS CHAR(24)
+AS
+BEGIN
+	RETURN CONVERT(CHAR(24),
+       CONVERT(VARBINARY(4), DATEDIFF(SECOND,'1970-01-01 00:00:00', GETUTCDATE())) +
+       CONVERT(VARBINARY(6), @rank) + 
+	   CONVERT(VARBINARY(2), @@SPID ^ @@CPU_BUSY ^ @@IDLE), 2)
+END
